e18c0b3aa216e93220c7f55de2100e1cd94adef3
rishik ashili <141920403+rishik-ashili@users.noreply.github.com>
2025-06-18 12:34:49 +0530

    1706

---
+++ 17 june/reversedigit.py
@@ -0,0 +1,7 @@
+#User function Template for python3
+class Solution:
+    def reverseDigits(self, n):
+        reversed_str = str(n)[::-1]
+        reversed_int = int(reversed_str)
+        
+        return reversed_int
